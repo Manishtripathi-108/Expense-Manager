90c77776163a87754aed76eb005f5a3f75fc6764
Manish Tripathi <manish82644@gmail.com>
2024-06-11 03:36:03 +0530

    Add initial project files and directories

---
+++ resources/views/includes/favicons.blade.php
@@ -0,0 +1,7 @@
+<link href="{{ asset('apple-touch-icon.png') }}" rel="apple-touch-icon" sizes="180x180">
+<link type="image/png" href="{{ asset('favicon-32x32.png') }}" rel="icon" sizes="32x32">
+<link type="image/png" href="{{ asset('favicon-16x16.png') }}" rel="icon" sizes="16x16">
+<link href="{{ asset('site.webmanifest') }}" rel="manifest">
+<link href="{{ asset('safari-pinned-tab.svg') }}" rel="mask-icon" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
